87faca3eb4d05ece9f8735e8eb3066647725f57f
moazzamriaz1 <98743892+moazzamriaz1@users.noreply.github.com>
2023-11-21 14:48:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import streamlit as st
 from model1 import model1
 from model2 import model2
+from model3 import model3
+
 
 def main():
     st.title("Text Classification App")
 
     # Model selection
-    model_selection = st.selectbox("Select Model", ["Model 1", "Model 2"])
+    model_selection = st.selectbox("Select Model", ["Model 1", "Model 2", "Model 3"])
 
     if model_selection == "Model 1":
-        model2()
+        model3()
     elif model_selection == "Model 2":
         model1()
+    elif model_selection == "Model 3":
+        model2()
 
 
 
